451fa4ba70071fe57bb7977b54e5611e7c12a4fd
Aiko Wessels <aiko.wessels@gmail.com>
2022-05-15 19:59:42 +0200

    feat: add rough project outline

---
+++ code/02_Example01.java
@@ -0,0 +1,11 @@
+
+public class Test {
+    // Dies ist Java Code in einer externen Datei
+    static int[] array = new int[7];
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i);
+        }
+    }
+}
